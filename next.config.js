e573f758343bb51abba5966389a7ee5aa5209706
ahhzaky <siahmadzaky@gmail.com>
2023-12-18 10:15:01 +0700

    Update config next image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // image from brucket gcs
+  images: {
+    domains: ['storage.googleapis.com'],
+  },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback.fs = false;
+  //   }
+  //   return config;
+  // }
+
 }
 
 module.exports = nextConfig
